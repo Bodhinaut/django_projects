7d6c5bd559283c0cdfeee7bab22ff1e1e73e8e49
Kyle Shive <shivekyle@gmail.com>
2018-06-18 14:00:12 -0500

    further along in django tutorial

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from first_app import views
+from django.conf.urls import include
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^first_app/', include('first_app.urls') ),
     path('admin/', admin.site.urls),
 ]
