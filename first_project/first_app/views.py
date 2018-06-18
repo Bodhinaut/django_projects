c3683881d635fe0837be33d05327016a16f1b270
Kyle Shive <shivekyle@gmail.com>
2018-06-18 18:53:52 -0500

    further in django tutorial

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World!")
+	my_dict = {'insert_me': "Coming from first_app/index.html"}
+	return render(request, 'first_app/index.html', context=my_dict)
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -20,7 +20,7 @@ from first_app import views
 from django.conf.urls import include
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	#url(r'^$', views.index, name='index'),
 	url(r'^first_app/', include('first_app.urls') ),
     path('admin/', admin.site.urls),
 ]
